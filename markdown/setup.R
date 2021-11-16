b2c5640bd0936cddb5ec3ee41db8a0a9195a381a
Leo <leonardombastos@gmail.com>
2021-11-16 12:00:27 -0600

    added package setup

---
+++ markdown/setup.R
@@ -0,0 +1,27 @@
+# Installing packages 
+# (do only once per package, or again to update a previously-installed package)
+install.packages("readxl") # for reading excel files
+install.packages("readr") # for fast reading/writing of files
+install.packages("janitor") # for data name cleaning
+install.packages("ggplot2") # for plotting
+install.packages("dplyr") # for data wrangling
+install.packages("tidyr") # for data wrangling
+install.packages("car") # for type 3 SS anova tables
+install.packages("broom") # for mixed-effect model diagnostics
+install.packages("ggthemes") # for extra options to plot styling
+install.packages("purrr") # for iteration
+
+# Loading packages
+# (do evertyime open a new R/RStudio window)
+library("readxl")
+library("readr")
+library("janitor")
+library("ggplot2")
+library("dplyr")
+library("tidyr")
+library("car")
+library("broom")
+library("ggthemes")
+library("purrr")
+
+
